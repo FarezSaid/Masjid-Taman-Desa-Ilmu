85650f28a7bdad535309396d2f802917de1fee89
Farez <farezsaid94@gmail.com>
2019-11-20 22:34:40 +0800

    Updated file

--- projectmasjid/index.php
+++ projectmasjid/index.php
@@ -22,17 +22,18 @@
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
+  <link href='https://fonts.googleapis.com/css?family=Cinzel Decorative' rel='stylesheet'>
+   <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
   <!-- Custom styles for this template -->
-  <link rel="stylesheet" href="css/social icon.css">
-  <link rel="stylesheet" href="css/icons.min.css">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/plugins.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="stylesheet" type="text/css" href="css/responsive.css">
-  <link rel="stylesheet" type="text/css" href="css/settings.css">
-  <link rel="stylesheet" type="text/css" href="css/navigation.css">
-
+  <link rel="stylesheet"  href="css/social icon.css">
+  <link rel="stylesheet"  href="css/icons.min.css">
+  <link rel="stylesheet"  href="css/bootstrap.min.css">
+  <link rel="stylesheet"  href="css/plugins.min.css">
+  <link rel="stylesheet"  href="css/style.css">
+  <link rel="stylesheet"  href="css/responsive.css">
+  <link rel="stylesheet"  href="css/settings.css">
+  <link rel="stylesheet"  href="css/navigation.css">
+  <link rel="stylesheet"  href="css/timer.css">
 </head>
 <body itemscope>
 <div class="preloader">
@@ -50,32 +51,31 @@
 <nav>
 <div>
 <ul>                            
-<li><a href="index.php" title="" itemprop="url">Utama</a>
-<li class="menu-item-has-children"><a href="#" title="" itemprop="url">Mengenai Kami</a>
+<li><a href="index.php" itemprop="url">Utama</a>
+<li class="menu-item-has-children"><a href="#"itemprop="url">Mengenai Kami</a>
 <ul>
-<li><a href="organisasi.html" title="" itemprop="url">Carta Organisasi</a></li>
-<li><a href="sejarah.html" title="" itemprop="url">Sejarah Masjid</a></li>
-<li><a href="visi.html" title="" itemprop="url">Visi & Misi</a></li>
-<li><a href="objektif.html" title="" itemprop="url">Fungsi dan Objektif</a></li>
+<li><a href="organisasi.html" itemprop="url">Carta Organisasi</a></li>
+<li><a href="sejarah.html" itemprop="url">Sejarah Masjid</a></li>
+<li><a href="visi.html" itemprop="url">Visi & Misi</a></li>
 </ul>
 </li>
-<li><a href="hubungi kami.html" title="" itemprop="url">Hubungi Kami</a>
-<li class="menu-item-has-children"><a href="#" title="" itemprop="url">Galeri</a>
+<li><a href="hubungi kami.html" itemprop="url">Hubungi Kami</a>
+<li class="menu-item-has-children"><a href="#"  itemprop="url">Galeri</a>
 <ul>
-<li><a href="fasiliti.html" title="" itemprop="url">Fasiliti Masjid</a></li>
-<li><a href="hadis.html" title="" itemprop="url">Hadis</a></li>
+<li><a href="fasiliti.html" itemprop="url">Fasiliti Masjid</a></li>
+<li><a href="http://www.islam.gov.my/e-hadith" itemprop="url">Hadis</a></li>
 </ul>
 </li>
-<li class="menu-item-has-children"><a href="#" title="" itemprop="url">Muat Turun</a>
+<li class="menu-item-has-children"><a href="#" itemprop="url">Muat Turun</a>
 <ul>
-<li><a href="http://www.islam.gov.my/e-khutbah" title="" itemprop="url">E-Khutbah</a></li>
-<li><a href="nota kuliah.html" title="" itemprop="url">Nota Kuliah</a></li>
+<li><a href="http://www.islam.gov.my/e-khutbah"  itemprop="url">E-Khutbah</a></li>
+<li><a href="nota kuliah.html" itemprop="url">Nota Kuliah</a></li>
 </ul>
 </li>
-<li><a href="cili.html" title="" itemprop="url">Fertigasi Cili</a>
-<li><a href="login.php" title="" itemprop="url"><center>Log In</center><small>Are you an Admin? </small></a></li>
+<li><a href="cili.html"  itemprop="url">Fertigasi Cili</a>
+<li><a href="login.php"  itemprop="url"><center>Log In</center><small>Are you an Admin? </small></a></li>
 </ul>
-<a class="srch-btn" href="#" title="" itemprop="url"><i class="fas fa-search"></i></a>
+<a class="srch-btn" href="#"  itemprop="url"><i class="fas fa-search"></i></a>
 </div>
 </nav>
 </div>
@@ -123,12 +123,11 @@
 <div class="col-md-6 col-sm-12 col-lg-6">
 <div class="abt-desc">
 <div class="sec-tl">
-<span class="theme-clr">Sejarah Kami</span>
-<h2 itemprop="headline">Tentang Masjid Taman Desa Ilmu</h2>
+<h4 itemprop="headline" style="font-size:27pt;color:#473d3d;font-weight: 600;font-family:Cinzel Decorative;">Selamat Datang Ke Laman Web Masjid Taman Desa Ilmu </h4><b>
 </div>
-<p itemprop="description">Ditubuhkan........</p>
-<p itemprop="description">Sambung ....</p>
-<a class="theme-btn theme-bg brd-rd5" href="sejarah masjid.html" title="" itemprop="url">READ MORE</a>
+<p itemprop="description"style="font-family:Roboto";>Masjid Taman Desa Ilmu<b> merupakan sebuah pusat komuniti islam yang sangat komited dan berdaya saing dalam memelihara dan menegakkan syiar islam.</p>
+<p itemprop="description" style="font-family:Roboto";>
+<a class="theme-btn theme-bg brd-rd5" href="sejarah.html" title="" itemprop="url" style="font-family:Cinzel Decorative;">Lanjut</a>
 </div>
 </div>
 </div>
@@ -138,10 +137,9 @@
 </section>
 <section>
 <div class="gap white-layer opc75">
-<!--<div class="fixed-bg" style="background-image: url(img/parallax3.jpg);"></div>-->
 <div class="container">
 <div class="sec-tl">
-<h2 itemprop="headline">Waktu Solat</h2>
+<h2 itemprop="headline"style="font-family:Cinzel Decorative;"><b>Waktu Solat</b></h2>
 <p><font color="white" id="demo"></font></p>
 </div>
 <div class="prayer-timing-wrp">
@@ -170,31 +168,31 @@
 <thead><tr><th><span>Solat</span></th><th><span>Waktu Solat</span></th></tr></thead>
 <tbody>
 <tr class="">
-<td><span>Imsak</span></td>
+<td><span style="font-family: Cinzel Decorative;">Imsak</span></td>
 <td class="Waktu-solat-Imsak">04:53 pagi</td>
 </tr>
 <tr>
-<td><span>Subuh</span></td>
+<td><span style="font-family: Cinzel Decorative;">Subuh</span></td>
 <td class="Waktu-solat-Subuh">05:03 pagi</td>
 </tr>
 <tr>
-<td><span>Syuruk</span></td>
+<td><span style="font-family: Cinzel Decorative;">Syuruk</span></td>
 <td class="Waktu-solat-Syuruk">06.20 pagi</td>
 </tr>
 <tr>
-<td><span>Zohor</span></td>
+<td><span style="font-family: Cinzel Decorative;">Zohor</span></td>
 <td class="Waktu-solat-Zohor">12:24 tengahari</td>
 </tr>
 <tr>
-<td><span>Asar</span></td>
+<td><span style="font-family: Cinzel Decorative;">Asar</span></td>
 <td class="Waktu-solat-Asar">03:41 petang</td>
 </tr>
 <tr>
-<td><span>Maghrib</span></td>
+<td><span style="font-family: Cinzel Decorative;">Maghrib</span></td>
 <td class="Waktu-solat-Maghrib">06:25 malam</td>
 </tr>
 <tr>
-<td><span>Ishaq</span></td>
+<td><span style="font-family: Cinzel Decorative;">Ishaq</span></td>
 <td class="Waktu-solat-Ishaq">07:35 malam</td>
 </tr>
 </tbody>
@@ -208,11 +206,81 @@
 </div>
 </section>
 <section>
+<div class="gap black-layer opc75">
+<div class="fixed-bg" style="background-image: url(assets/images/parallax2.jpg);"></div>
+<div class="container">
+<div class="sec-tl text-center">
+<h2 itemprop="headline" style="font-family: Cinzel Decorative;"><b>Matlamat Kami</b></h2>
+</div>
+<div class="remove-ext3">
+<ul class="plrs-wrp text-center">
+<li>
+<div class="plr-bx">
+<i class="brd-rd50"><img src="a.PNG"></i>
+<h5 itemprop="headline">Ilmu</h5>
+</div>
+</li>
+<li>
+<div class="plr-bx">
+<i class="brd-rd50"><img src="b.png"></i>
+<h5 itemprop="headline">Ehsan</h5>
+</div>
+</li>
+<li>
+<div class="plr-bx">
+<i class="brd-rd50"><img src="c.png"></i>
+<h5 itemprop="headline">Komuniti</h5>
+</div>
+</li>
+<li>
+<div class="plr-bx">
+<i class="brd-rd50"><img src="d.png"></i>
+<h5 itemprop="headline">Khidmat</h5>
+</div>
+</li>
+<li>
+<div class="plr-bx">
+<i class="brd-rd50"><img src="4i-300x264.png"></i>
+ <h5 itemprop="headline">Kebajikan</h5>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</section>
+<section>
+<div class="gap black-layer opc75">
+<div class="fixed-bg" style="background-image: url(assets/images/parallax2.jpg);"></div>
+<div class="container">
+<div class="ramadanHeadibng">
+ 
+  <!-- Generator: Adobe Illustrator 22.0.0, SVG Export Plug-In  -->
+<svg version="1.1"
+   xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
+   x="0px" y="0px" width="100%" height="93.9px" viewBox="0 0 414.1 93.9" style="enable-background:new 0 0 414.1 93.9;" 
+   xml:space="preserve">
+<style type="text/css">
+  .st0{fill-rule:evenodd;clip-rule:evenodd;fill:#CCB77C;}
+  .st1{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#304169;stroke-width:13;stroke-linejoin:round;stroke-miterlimit:10;}
+  .st2{fill-rule:evenodd;clip-rule:evenodd;fill:none;stroke:#304169;stroke-width:7;stroke-linejoin:round;stroke-miterlimit:10;}
+</style>
+<defs>
+</defs>
+</svg> 
+</div>
+<div class="container">
+<h1 class="title">Time left until Ramadan 2020</h1>
+<h1 id="ramadanTimer"></h1>      
+</div>
+</div>
+</div>
+</section>
+<section>
 <div class="gap">
 <div class="container">
 <div class="sec-tl text-center">
-<span class="theme-clr">Acara</span>
-<h2 itemprop="headline">Acara & Tentatif</h2>
+<h2 itemprop="headline"style="font-family: Cinzel Decorative;"><b>Acara & Tentatif</b></h2>
 <div class="content">
 			<h2 class="content-title"></h2>
 			<hr>
@@ -266,38 +334,35 @@
 
 <div class="col-md-3 col-sm-6 col-lg-3">
 <div class="widget">
-<h5 itemprop="headline">Terkini</h5>
+<h5 itemprop="headline" style="font-family: Cinzel Decorative;">Sosial</h5>
 <div class="rcnt-wrp">
 <div class="rcnt-bx">
-<a class="brd-rd5" href="blog-detail.html" title="" itemprop="url"><img src="img/post-img1-1.jpg" alt="rcnt-img1.jpg" itemprop="image"></a>
+<div class="center1">
 <div class="rcnt-inf">
-<h6 itemprop="headline"><a href="blog-detail.html" title="" itemprop="url">Help Poor People</a></h6>
-<span class="theme-clr"><i class="far fa-calendar-alt"></i>Sept 09, 2018</span>
 </div>
 </div>
-<div class="rcnt-bx">
-<a class="brd-rd5" href="blog-detail.html" title="" itemprop="url"><img src="img/post-img1-2.jpg" alt="rcnt-img2.jpg" itemprop="image"></a>
-<div class="rcnt-inf">
-<h6 itemprop="headline"><a href="blog-detail.html" title="" itemprop="url">Learn Modern Islam</a></h6>
-<span class="theme-clr"><i class="far fa-calendar-alt"></i>Sept 05, 2018</span>
 </div>
-</div>
+<a href="https://www.facebook.com/MTDI94300/" title="Facebook" itemprop="url" target="_blank"><i class="fa fa-facebook-f"></i></a>&nbsp;
+<a href="#" title="Instagram" itemprop="url" target="_blank"><i class="fa fa-instagram"></i></a>&nbsp; &nbsp;
+<a href="#" title="Youtube" itemprop="url" target="_blank"><i class="fa fa-youtube"></i></a>
 </div>
 </div>
 </div>
 <div class="col-md-3 col-sm-6 col-lg-3">
 <div class="widget">
-<h5 itemprop="headline">Hubungi</h5>
+<h5 itemprop="headline" style="font-family: Cinzel Decorative;">Menu</h5>
 <div class="row mrg10">
 <div class="col-md-12 col-sm-12 col-lg-12">
-<p itemprop="description">Masjid Taman Desa Ilmu<br>
-Lot 5868 / Sub Lot 8, Lorong 22<br>
-Taman Desa Ilmu Kota Samarahan<br>
-Jalan Datuk Mohd Musa 94300 Kota Samarahan<br>
+<itemprop="description"><a href="index.php" itemprop="url"style="font-family:Cinzel Decorative";>Utama</a><br>
+<a href="sejarah.html" itemprop="url"style="font-family:Cinzel Decorative";>Sejarah Masjid</a><br>
+<a href="visi.html" itemprop="url"style="font-family:Cinzel Decorative";>Visi & Misi</a><br>
+<a href="hubungi kami.html" itemprop="url"style="font-family:Cinzel Decorative";>Hubungi Kami</a><br>
+<a href="fasiliti.html" itemprop="url"style="font-family:Cinzel Decorative";>Fasiliti Masjid</a><br>
+<a href="http://www.islam.gov.my/e-hadith" itemprop="url"style="font-family:Cinzel Decorative";>Hadis</a><br>
+<a href="http://www.islam.gov.my/e-khutbah"  itemprop="url"style="font-family:Cinzel Decorative";>E-Khutbah</a><br>
+<a href="nota kuliah.html" itemprop="url"style="font-family:Cinzel Decorative";>Nota Kuliah</a><br>
+<a href="cili.html"  itemprop="url"style="font-family:Cinzel Decorative";>Fertigasi Cili</a>
 </p>
-<a href="https://www.facebook.com/MTDI94300/" title="Facebook" itemprop="url" target="_blank"><i class="fa fa-facebook-f"></i></a>&nbsp;
-<a href="#" title="Instagram" itemprop="url" target="_blank"><i class="fa fa-instagram"></i></a>&nbsp; &nbsp;
-<a href="#" title="Youtube" itemprop="url" target="_blank"><i class="fa fa-youtube"></i></a>
 </div>
 </div>
 </div>
@@ -335,7 +400,7 @@ Jalan Datuk Mohd Musa 94300 Kota Samarahan<br>
 </div>
 </div>
 </section>
-<p style="text-align: center;"><strong>Copyright © 2019 Masjid Taman Desa Ilmu</strong></p>
+<p style="text-align: center; font-family: Cinzel Decorative;"><strong>Copyright © 2019 Masjid Taman Desa Ilmu</strong></p>
     <!-- /.container -->
   </footer>
 </main>
@@ -347,6 +412,7 @@ Jalan Datuk Mohd Musa 94300 Kota Samarahan<br>
  <script src="js/plugins.min.js"></script>
  <script src="js/custom-scripts.js"></script>
  <script src="js/namaz.js"></script>
+ <script src="js/timer.js"></script>
  <script>
 var today = new Date();
 var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
@@ -364,6 +430,12 @@ document.getElementById("demo").innerHTML = dateTime;
   margin-right: auto;
   width: 35%;
 }
+.center1 {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+}
 .column {
   float: left;
   width: 33%;
@@ -375,5 +447,4 @@ document.getElementById("demo").innerHTML = dateTime;
 }
 </style>
 </body>
-
 </html>
